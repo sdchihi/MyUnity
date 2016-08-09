9d14e960856a122a44db62e802022637b93d2f08
sdchihi <sdchihi@naver.com>
2016-08-09 18:55:20 +0900

    character anim & map & camera

---
+++ Second/Assets/01.Script/FollowCam.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour
+{
+    public Transform targetTr;
+    public float dist = 3.0f;
+    public float height = 1.0f;
+    public float dampTraace = 10.0f;
+
+    private Transform tr;
+
+    // Use this for initialization
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        tr.position = Vector3.Lerp(tr.position,new Vector3(targetTr.position.x-dist, targetTr.position.y+height,targetTr.position.z) , Time.deltaTime * dampTraace);
+        //x-3 y+1
+        tr.LookAt(targetTr.position);
+    }
+}
---
+++ Second/Assets/01.Script/PlayerCtr.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCtr : MonoBehaviour {
+
+    private float h = 0.0f;
+    private float v = 0.0f;
+
+    private Transform tr;
+    public float moveSpeed=10.0f;
+
+    public enum PlayerState { idle, walk, gotHit, attack, die};
+
+
+    public PlayerState monsterState = PlayerState.idle;
+
+    private bool isDie = false;
+
+    private Animator animator;
+
+    
+
+	void Start () {
+        tr = GetComponent<Transform>();
+
+        animator = this.gameObject.GetComponent<Animator>();
+	}
+	
+
+
+	void Update ()
+    {
+        
+
+        if (animator.GetBool("IsAttack"))
+        {
+            v = 0;
+            h = 0;
+        }
+        else {
+            v = -Input.GetAxis("Horizontal");
+            h = Input.GetAxis("Vertical");
+        }
+        Vector3 moveForward = ((Vector3.forward * v) + (Vector3.forward * h)).normalized;
+
+
+
+        if (v >= 0.1f)
+        {
+            tr.rotation = Quaternion.Euler(0, 0, 0);
+            moveForward = Vector3.forward * v;
+            if (h >= 0.1f)
+            {
+                tr.rotation = Quaternion.Euler(0, 45, 0);
+                moveForward = Vector3.forward * h + Vector3.forward * v;
+            }
+            else if (h <= -0.1f)
+            {
+                tr.rotation = Quaternion.Euler(0, -45, 0);
+                moveForward = Vector3.back * h + Vector3.forward * v;
+            }
+            animator.SetBool("IsWalk", true);
+        }
+        else if (v <= -0.1f)
+        {
+            tr.rotation = Quaternion.Euler(0, 180, 0);
+            moveForward = Vector3.back * v;
+            if (h >= 0.1f)
+            {
+                tr.rotation = Quaternion.Euler(0, 135, 0);
+                moveForward = Vector3.forward * h + Vector3.back * v;
+            }
+            else if (h <= -0.1f)
+            {
+                tr.rotation = Quaternion.Euler(0, 225, 0);
+                moveForward = Vector3.back * h + Vector3.back * v;
+            }
+            animator.SetBool("IsWalk", true);
+        }
+        else if (h >= 0.1f)
+        {
+            tr.rotation = Quaternion.Euler(0, 90, 0);
+            moveForward = Vector3.forward * h;
+            animator.SetBool("IsWalk", true);
+        }
+        else if (h <= -0.1f)
+        {
+            tr.rotation = Quaternion.Euler(0, -90, 0);
+            moveForward = Vector3.back * h;
+            animator.SetBool("IsWalk", true);
+        }
+        else
+        {
+            animator.SetBool("IsWalk", false);
+        }
+
+
+        tr.Translate(moveForward.normalized * Time.deltaTime * moveSpeed, Space.Self);
+
+
+        if (!animator.GetBool("IsAttack")) {
+            if (Input.GetMouseButtonDown(0))
+            {
+                StartCoroutine(this.PlayerAttack());
+            }
+        }
+        
+        
+    }
+
+
+    IEnumerator PlayerAttack()
+    {
+        animator.SetBool("IsAttack", true);
+        
+        yield return new WaitForSeconds(0.8f);
+
+        animator.SetBool("IsAttack", false);
+    }
+
+
+}
