dd0dffbb3d37323bdf74a26270a2b6333941fe10
sdchihi <sdchihi@naver.com>
2016-08-21 22:56:54 +0900

    Tile map & Editor

---
+++ Third/Assets/Editor/MapEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+//이 에디터 스크립트가 어떤클래스 , 스크립트를 다루는지 명시해야함  밑에
+[CustomEditor (typeof (MapGenerator))]
+public class MapEditor : Editor {
+    //에디터를 사용함으로써 굳이 게임을 실행하지 않더라도 리얼타임으로 상태를 갱신할수있다.
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        MapGenerator map = target as MapGenerator;      //CustomEditor로 다룰거라고 선언한 오브젝트는 target으로 접근이 가능하다.
+
+        map.GenerateMap();
+    }
+
+}
---
+++ Third/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour
+{
+
+    public Transform tilePrefab;
+    public Vector2 mapSize;
+
+    [Range(0,1)]
+    public float outLinePercent;
+
+    void Start() {
+        GenerateMap();
+    }
+
+    public void GenerateMap() {
+
+        string holderName = "Generated map";
+        if (transform.FindChild(holderName))
+        {
+            DestroyImmediate(transform.FindChild(holderName).gameObject);       //에디터에서 실행할거라서 destroy가아닌 destroyImmediate
+        }
+
+        Transform mapHolder = new GameObject(holderName).transform;
+        mapHolder.parent = transform;
+
+
+        for (int x = 0; x < mapSize.x; x++) {
+            for (int y = 0; y < mapSize.y; y++) {
+                //맵을 생성할 위치설정
+                Vector3 tilePosition = new Vector3(-mapSize.x / 2 + 0.5f + x, 0, -mapSize.y/2 + 0.5f + y);
+                Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right *90)) as Transform;
+                newTile.localScale = Vector3.one * (1 - outLinePercent);    //테두리 결정
+
+                newTile.parent = mapHolder;
+            }
+        }
+    }
+
+
+
+
+}
+
+
